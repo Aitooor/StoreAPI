8f41cad484245b0fffd8c5633f7fd8f9ac7ef738
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-06-16 22:10:49 -0300

    feat: moved to MongoDB

--- src/main/java/online/nasgar/store/StorePlugin.java
+++ src/main/java/online/nasgar/store/StorePlugin.java
@@ -1,8 +1,10 @@
 package online.nasgar.store;
 
 import lombok.Getter;
+import online.nasgar.store.config.ConfigFile;
 import online.nasgar.store.listener.PlayerListener;
-import online.nasgar.store.redis.StoreRedisDatabase;
+import online.nasgar.store.mongo.MongoDB;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StorePlugin extends JavaPlugin {
@@ -13,7 +15,10 @@ public class StorePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        new StoreRedisDatabase();
+        new MongoDB();
         new PlayerListener();
+        double seconds = new ConfigFile(this, "config").getDouble("runnable.seconds");
+        // minutes * 20 = seconds * 20 (ticks)
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new MongoDB.mongoRunnable(), (long) seconds * 20, (long) seconds * 20);
     }
 }
---
+++ src/main/java/online/nasgar/store/mongo/MongoDB.java
@@ -0,0 +1,89 @@
+package online.nasgar.store.mongo;
+
+import com.mongodb.client.*;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
+import online.nasgar.store.StorePlugin;
+import online.nasgar.store.config.ConfigFile;
+import online.nasgar.store.storage.Storage;
+import online.nasgar.store.utils.Utils;
+import org.bson.BsonDocument;
+import org.bson.Document;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MongoDB {
+
+    private static MongoDatabase mongoDb;
+    private static MongoCollection<Document> mongoCol;
+
+    private String AUTH_URL = "mongodb://%s:%s@%s:%s/?authMechanism=DEFAULT";
+    private String NO_AUTH_URL = "mongodb://%s:%s/";
+
+    public MongoDB() {
+        connect();
+    }
+
+    public void connect() {
+        Utils.async(() -> {
+            ConfigFile config = new ConfigFile(StorePlugin.getInstance(), "config");
+            String user = config.getString("mongo.user"), password = config.getString("mongo.password"),
+                    address = config.getString("mongo.address"), port = config.getString("mongo.port"),
+                    database = config.getString("mongo.database"), collection = config.getString("mongo.collection");
+
+            try {
+                MongoClient mongoClient = MongoClients.create(user == null || password == null ?
+                        String.format(NO_AUTH_URL, address, port) : String.format(AUTH_URL, user, password, address, port));
+                mongoDb = mongoClient.getDatabase(database);
+                mongoCol = mongoDb.getCollection(collection);
+
+                Bukkit.getLogger().info("[StoreAPI] " + ChatColor.GREEN + "Connected to MongoDB!");
+            } catch (Exception e) {
+                Bukkit.getLogger().info("[StoreAPI] " + ChatColor.RED + "The plugin can't reach the MongoDB connection");
+            }
+        });
+    }
+
+    public static UpdateResult saveDocuments() {
+        return mongoCol.updateMany(
+                Filters.and(
+                        Filters.eq("used", false),
+                        Filters.eq("paid", true)
+                ),
+                Updates.set("used", true)
+        );
+    }
+
+    public static FindIterable<Document> getProducts() {
+        return mongoCol.find(BsonDocument.parse("{paid: true, used: false}"));
+    }
+
+    public static class mongoRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            Utils.async(() -> {
+                MongoDB.getProducts().forEach(doc -> {
+                    OfflinePlayer p = Bukkit.getOfflinePlayer(doc.getString("name"));
+                    List<String> commands = doc.getList("commands", String.class);
+                    if (!commands.isEmpty()) {
+                        if (!p.isOnline())
+                            Storage.save(p.getName(), commands);
+                        else
+                            Utils.sync(() -> commands.forEach(command -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                                    command.replace("<player>", p.getName()))));
+                    }
+
+                });
+
+                Bukkit.getLogger().info("[StoreAPI] Modified document count: " + MongoDB.saveDocuments().getModifiedCount());
+            });
+        }
+    }
+}
