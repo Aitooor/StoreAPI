8029ef73438d9a45d9aa7bdba8b41517dac21527
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-06-30 14:35:39 -0300

    fix: modified MongoDB login information to be a link

--- src/main/java/online/nasgar/store/mongo/MongoDB.java
+++ src/main/java/online/nasgar/store/mongo/MongoDB.java
@@ -20,8 +20,6 @@ public class MongoDB {
     private static final String SERVER_NAME = new ConfigFile(StorePlugin.getInstance(), "config").getString("server_name");
     private static MongoDatabase mongoDb;
     private static MongoCollection<Document> mongoCol;
-    private final String AUTH_URL = "mongodb" + srv() + "://%s:%s@%s:%s/" + params();
-    private final String NO_AUTH_URL = "mongodb" + srv() + "://%s:%s/" + params();
 
     public MongoDB() {
         connect();
@@ -49,13 +47,10 @@ public class MongoDB {
     public void connect() {
         Utils.async(() -> {
             ConfigFile config = new ConfigFile(StorePlugin.getInstance(), "config");
-            String user = config.getString("mongo.user"), password = config.getString("mongo.password"),
-                    address = config.getString("mongo.address"), port = config.getString("mongo.port"),
-                    database = config.getString("mongo.database"), collection = config.getString("mongo.collection");
+            String database = config.getString("mongo.database"), collection = config.getString("mongo.collection");
 
             try {
-                MongoClient mongoClient = MongoClients.create(user == null || password == null ?
-                        String.format(NO_AUTH_URL, address, port) : String.format(AUTH_URL, user, password, address, port));
+                MongoClient mongoClient = MongoClients.create(config.getString("mongo.link"));
                 mongoDb = mongoClient.getDatabase(database);
                 mongoCol = mongoDb.getCollection(collection);
 
@@ -64,15 +59,6 @@ public class MongoDB {
                 Bukkit.getLogger().info("[StoreAPI] " + ChatColor.RED + "The plugin can't reach the MongoDB connection");
             }
         });
-    }
-
-    private String srv() {
-        return new ConfigFile(StorePlugin.getInstance(), "config").getBoolean("mongo.srv") ? "" : "srv";
-    }
-
-    private String params() {
-        String params = new ConfigFile(StorePlugin.getInstance(), "config").getString("mongo.params");
-        return params == null || params.equals("") || params.equals(" ") ? "" : "?" + params;
     }
 
     public static class mongoRunnable implements Runnable {
