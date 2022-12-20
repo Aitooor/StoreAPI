9b29eea3bf55a8e1e76c69d23a8855ed33d43cb3
Leo_S <71842101+Leonardo-shitp@users.noreply.github.com>
2022-12-19 22:12:34 -0500

    Fixed send request

--- src/main/java/online/nasgar/store/gson/RequestGson.java
+++ src/main/java/online/nasgar/store/gson/RequestGson.java
@@ -10,6 +10,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 
+import static org.bukkit.Bukkit.getLogger;
+
 @SuppressWarnings({"unused", "deprecation"})
 public class RequestGson {
 
@@ -17,24 +19,26 @@ public class RequestGson {
 
     public static String products = "[]";
     public static BukkitRunnable redisRunnable = new BukkitRunnable() {
-
         @Override
         public void run() {
             Gson gson = new Gson();
             Product[] objects = gson.fromJson(products, Product[].class);
+
             for(Product object : objects) {
                 List<String> commands = object.getCommands();
-                if(!commands.isEmpty()) {
+                if(commands != null && !commands.isEmpty()) {
                     if(!Bukkit.getOfflinePlayer(object.getPlayer()).isOnline())
                         Storage.save(object.getPlayer(), commands);
                     else
                         Bukkit.getScheduler().runTask(StorePlugin.getInstance(), () -> commands.forEach(command -> {
                             command = PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(object.getPlayer()), command);
-                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("<player>", object.getPlayer()));
+                            getLogger().info(command);
+                            command = command.replace("<player>", object.getPlayer());
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
                         }));
                 }
             }
-            Bukkit.getLogger().info("[StoreAPI] Modified document count: " + objects.length);
+            getLogger().info("[StoreAPI] Modified document count: " + objects.length);
             this.cancel();
         }
     };
